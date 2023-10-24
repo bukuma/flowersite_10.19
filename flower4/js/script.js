3ea7210c1d6f57ae0307979d577452bd4b68c207
ChisaSaitou <rkhnd@outlook.jp>
2023-10-24 17:16:46 +0900

    10.24

--- flower4/js/script.js
+++ flower4/js/script.js
@@ -1,3 +1,15 @@
 $(".openbtn4").click(function () {
     $(this).toggleClass('active');
 });
+
+const heading = document.querySelector('#heading');
+const keyframes = {
+    opacity: [0, 1],
+    translate: ['0 50px', 0],
+};
+const options = {
+    duration: 2000,
+    easing: 'ease',
+};
+
+heading.animate(keyframes, options);
